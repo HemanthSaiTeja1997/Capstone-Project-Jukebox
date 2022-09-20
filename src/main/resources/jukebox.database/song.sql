0afaa351788b4220314565a3f107978ae50916aa
Hemanth Sai Teja <hemanthsaiteja881@gmail.com>
2022-09-20 21:43:30 +0530

    created a table song in the jukebox database

--- src/main/resources/jukebox.database/song.sql
+++ src/main/resources/jukebox.database/song.sql
@@ -2,3 +2,13 @@
 create DATABASE IF NOT EXISTS `jukebox`;
 --use the database by following below statement
 USE `jukebox`;
+--create a song table in the  jukebox database
+create TABLE `jukebox`.`song`(
+  `songId` INT NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
+  `album` VARCHAR(100) NOT NULL,
+  `artist` VARCHAR(45) NOT NULL,
+  `gener` VARCHAR(45) NOT NULL,
+  `duration` VARCHAR(45) NOT NULL,
+  `url` VARCHAR(200) NOT NULL,
+  PRIMARY KEY (`songId`));
