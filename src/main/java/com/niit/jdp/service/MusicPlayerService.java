68f20a300faff3ccaea5599ef0d2df408444aca4
Hemanth Sai Teja <hemanthsaiteja881@gmail.com>
2022-09-20 23:33:05 +0530

    create a file object that contains songpath

--- src/main/java/com/niit/jdp/service/MusicPlayerService.java
+++ src/main/java/com/niit/jdp/service/MusicPlayerService.java
@@ -6,8 +6,12 @@
 
 package com.niit.jdp.service;
 
+import java.io.File;
+
 public class MusicPlayerService {
     public void play(String songPath) {
+// 2. a file object that contains our song
+        File songFile = new File(songPath);
 
     }
 }
