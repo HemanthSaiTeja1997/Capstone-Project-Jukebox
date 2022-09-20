a64817650695a963f54666332508385da6bb31c5
Hemanth Sai Teja <hemanthsaiteja881@gmail.com>
2022-09-20 23:38:02 +0530

    create a object of AudioInputStream class that songFile

--- src/main/java/com/niit/jdp/service/MusicPlayerService.java
+++ src/main/java/com/niit/jdp/service/MusicPlayerService.java
@@ -6,12 +6,21 @@
 
 package com.niit.jdp.service;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.File;
+import java.io.IOException;
 
 public class MusicPlayerService {
     public void play(String songPath) {
-// 2. a file object that contains our song
+        // 2. a file object that contains our song
         File songFile = new File(songPath);
-
+        // 3. an object of the AudioInputStream class
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(songFile);
+        } catch (UnsupportedAudioFileException | IOException exception) {
+            System.err.println(exception.getMessage());
+        }
     }
 }
