2bea1e9c3cffe9ecb6bb1e23a5a12b9d08cb0398
Hemanth Sai Teja <hemanthsaiteja881@gmail.com>
2022-09-21 10:41:51 +0530

    added the songs in the resources

--- src/main/java/com/niit/jdp/JukeboxImplementation.java
+++ src/main/java/com/niit/jdp/JukeboxImplementation.java
@@ -6,8 +6,11 @@
 
 package com.niit.jdp;
 
+import com.niit.jdp.service.MusicPlayerService;
+
 public class JukeboxImplementation {
     public static void main(String[] args) {
-
+        MusicPlayerService musicPlayerService = new MusicPlayerService();
+        musicPlayerService.play("src/main/resources/songs/akatsuki-theme.wav");
     }
 }
