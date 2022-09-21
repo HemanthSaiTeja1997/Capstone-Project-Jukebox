f32f13d265ce8822773d4861987e64fc9b04d533
Hemanth Sai Teja <hemanthsaiteja881@gmail.com>
2022-09-21 18:09:04 +0530

    added all the methodology in SearchByArtistSortByName

--- src/main/java/com/niit/jdp/JukeboxImplementation.java
+++ src/main/java/com/niit/jdp/JukeboxImplementation.java
@@ -6,11 +6,23 @@
 
 package com.niit.jdp;
 
+import com.niit.jdp.repository.SongRepository;
+import com.niit.jdp.service.DatabaseService;
 import com.niit.jdp.service.MusicPlayerService;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class JukeboxImplementation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        DatabaseService databaseService = new DatabaseService();
+        databaseService.connect();
+        Connection connection = databaseService.getConnection();
+        SongRepository songRepository = new SongRepository();
         MusicPlayerService musicPlayerService = new MusicPlayerService();
-        musicPlayerService.play("src/main/resources/songs/akatsuki-theme.wav");
+        // musicPlayerService.play("src/main/resources/songs/akatsuki-theme.wav");
+        // songRepository.getAllSongs(connection).forEach(System.out::println);
+        songRepository.searchAndSortByArtist(connection, "Yasuharu Takanashi").forEach(System.out::println);
+
     }
 }
--- src/main/java/com/niit/jdp/repository/SongRepository.java
+++ src/main/java/com/niit/jdp/repository/SongRepository.java
@@ -17,30 +17,9 @@ import java.util.Comparator;
 import java.util.List;
 
 public class SongRepository {
-    List<Song> sortedSongs = new ArrayList<>();
 
-    public List<Song> getAllSongs(Connection connection) throws SQLException {
-        List<Song> songList = new ArrayList<>();
-        //create SQL query to retrieve all the rows from the Song table
-        String selectAllSongs = "SELECT * FROM `jukebox`.`song`;";
-        //use the connection object to execute the query for selecting all songs
-        ResultSet resultSet = connection.createStatement().executeQuery(selectAllSongs);
-        //iterate over the result set and create a Song object for each row
-        while (resultSet.next()) {
-            int songId = resultSet.getInt("song_Id");
-            String songName = resultSet.getString("name");
-            String album = resultSet.getString("album");
-            String artistName = resultSet.getString("artist_name");
-            String gener = resultSet.getString("genre");
-            String duration = resultSet.getString("duration");
-            String uRL = resultSet.getString("url");
-            //create a Song object using the values fetched from the result set
-            Song newSong = new Song(songId, songName, album, artistName, gener, duration, uRL);
-            songList.add(newSong);
-        }
 
-        return songList;
-    }
+    List<Song> sortedsong = new ArrayList<>();
 
     public boolean addSongDetails(Connection connection, Song song) throws SQLException {
         // 1. write the query for inserting a new Song object into the `song` table
@@ -62,13 +41,36 @@ public class SongRepository {
         return numberOfRowsAffected > 0;
     }
 
-    public Song searchAndSortByArtist(Connection connection, String artist) {
+    public List<Song> getAllSongs(Connection connection) throws SQLException {
+        List<Song> songList = new ArrayList<>();
+        //create SQL query to retrieve all the rows from the Song table
+        String selectAllSongs = "SELECT * FROM `jukebox`.`song`;";
+        //use the connection object to execute the query for selecting all songs
+        ResultSet resultSet = connection.createStatement().executeQuery(selectAllSongs);
+        //iterate over the result set and create a Song object for each row
+        while (resultSet.next()) {
+            int songId = resultSet.getInt("song_Id");
+            String songName = resultSet.getString("name");
+            String album = resultSet.getString("album");
+            String artistName = resultSet.getString("artist");
+            String gener = resultSet.getString("genre");
+            String duration = resultSet.getString("duration");
+            String uRL = resultSet.getString("url");
+            //create a Song object using the values fetched from the result set
+            Song newSong = new Song(songId, songName, album, artistName, gener, duration, uRL);
+            songList.add(newSong);
+        }
+
+        return songList;
+    }
+
+    public List<Song> searchByArtistAndSortByName(Connection connection, String artist) throws SQLException {
         // 1. write the query for selecting a song object from the `song` table
+
         String searchQuery = "SELECT*From `jukebox`.`song` where (`artist`=?);";
-        Song song = new Song();
+        List<Song> songList = new ArrayList<>();
         // 2. create a statement object
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(searchQuery);
+        try (PreparedStatement preparedStatement = connection.prepareStatement(searchQuery)) {
             // 3. set the values of the query parameters
             preparedStatement.setString(1, artist);
             //4. execute the query
@@ -79,21 +81,18 @@ public class SongRepository {
                 int songId = resultSet.getInt("song_Id");
                 String songName = resultSet.getString("name");
                 String album = resultSet.getString("album");
-                String artistName = resultSet.getString("artist_name");
+                String artistName = resultSet.getString("artist");
                 String gener = resultSet.getString("genre");
                 String duration = resultSet.getString("duration");
                 String uRL = resultSet.getString("url");
                 // 7. create a song object using the values fetched from the result set
-                song = new Song(songId, songName, album, artistName, gener, duration, uRL);
-                sortedSongs.add(song);
+                Song song = new Song(songId, songName, album, artistName, gener, duration, uRL);
+                songList.add(song);
+                songList.sort(Comparator.comparing(Song::getName));
             }
-            sortedSongs.sort(Comparator.comparing(Song::getArtist));
 
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            return songList;
         }
-
-
-        return song;
     }
+
 }
