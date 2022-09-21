0e7223709c92675f9090d2fc89bdf6bb518a4860
Hemanth Sai Teja <hemanthsaiteja881@gmail.com>
2022-09-21 11:01:35 +0530

    created a getAllSongs method in the SongRepository class

--- src/main/java/com/niit/jdp/repository/SongRepository.java
+++ src/main/java/com/niit/jdp/repository/SongRepository.java
@@ -6,5 +6,18 @@
 
 package com.niit.jdp.repository;
 
+import com.niit.jdp.model.Song;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SongRepository {
+    public List<Song> getAllSongs(Connection connection) throws SQLException {
+        List<Song> songList = new ArrayList<>();
+        //create SQL query to retrieve all the rows from the Song table
+
+        return null;
+    }
 }
