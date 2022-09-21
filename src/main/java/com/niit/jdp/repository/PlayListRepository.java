c6b73a7d9e56676fc15631119530e5309ce4f33a
Hemanth Sai Teja <hemanthsaiteja881@gmail.com>
2022-09-22 03:16:12 +0530

    create the statment object and set the values of query parameter

--- src/main/java/com/niit/jdp/repository/PlayListRepository.java
+++ src/main/java/com/niit/jdp/repository/PlayListRepository.java
@@ -10,19 +10,29 @@ import com.niit.jdp.model.PlayList;
 import com.niit.jdp.model.Song;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class PlayListRepository implements Repository<PlayList> {
+    List<Song> song = new ArrayList<Song>();
 
     @Override
-    public boolean addSongDetails(Connection connection, PlayList object) throws SQLException {
+    public boolean addSongDetails(Connection connection, PlayList playList) throws SQLException {
         // 1. write the query for inserting a new Song object into the `playlist` table
         String insertQuery = "INSERT INTO `jukebox`.`playlist` (`playlistId`, `playlistName`, `song_Id`) VALUES (?,?,?);";
         // 2. create a statement object
-
-
-        return false;
+        int numberOfRowsAffected;
+        String songID = song.toString().trim().replace("\\[\\]", "");
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+            // 3. set the values of the query parameters
+            preparedStatement.setInt(1, playList.getPlayListId());
+            preparedStatement.setString(2, playList.getPlayListName());
+            preparedStatement.setString(3, songID);
+            numberOfRowsAffected = preparedStatement.executeUpdate();
+        }
+        return numberOfRowsAffected > 0;
     }
 
     @Override
