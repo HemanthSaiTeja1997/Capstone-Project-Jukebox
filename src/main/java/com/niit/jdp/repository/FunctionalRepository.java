bd1c756d2222eecc99a5088021d15cd9dc8c8a32
Hemanth Sai Teja <hemanthsaiteja881@gmail.com>
2022-09-26 12:17:40 +0530

    final commit

--- src/main/java/com/niit/jdp/repository/FunctionalRepository.java
+++ src/main/java/com/niit/jdp/repository/FunctionalRepository.java
@@ -5,5 +5,5 @@ import java.sql.SQLException;
 
 @FunctionalInterface
 public interface FunctionalRepository {
-    public void toDisplaySelectedSongFromPlaylist(Connection connection, String playlistsName) throws SQLException;
+     void toDisplaySelectedSongFromPlaylist(Connection connection, String playlistsName) throws SQLException;
 }
