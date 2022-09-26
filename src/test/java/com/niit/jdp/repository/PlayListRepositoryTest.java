110453699c7727233a2b3d865e5d54cb190215a9
Hemanth Sai Teja <hemanthsaiteja881@gmail.com>
2022-09-26 07:02:31 +0530

    added a test case for getURLSUCCESS in PlayListRepositoryTest

--- src/test/java/com/niit/jdp/repository/PlayListRepositoryTest.java
+++ src/test/java/com/niit/jdp/repository/PlayListRepositoryTest.java
@@ -3,8 +3,12 @@ package com.niit.jdp.repository;
 import com.niit.jdp.model.PlayList;
 import com.niit.jdp.service.DatabaseService;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 class PlayListRepositoryTest {
     PlayListRepository playListRepository;
@@ -21,13 +25,19 @@ class PlayListRepositoryTest {
 
     @AfterEach
     void tearDown() {
+        playlist = null;
+        databaseService = null;
+        playListRepository = null;
     }
 
     @Test
-    void toDisplaySelectedSongFromPlaylist() {
+    void getURLSuccess() throws SQLException, ClassNotFoundException {
+        databaseService.connect();
+        Connection connection = databaseService.getConnection();
+        String expectedOutput = "src/main/resources/songs/03-Homecoming.wav";
+        String actualOutput = playListRepository.getURL(connection, 3);
+        Assertions.assertEquals(expectedOutput, actualOutput);
     }
 
-    @Test
-    void getURL() {
-    }
+
 }
